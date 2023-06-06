189fd1ceba558cbb1c5fd16adcaf736b42e36e34
ZhannaTursunbekova <tursunbekova.zhanna@gmail.com>
2023-06-06 15:27:34 +0300

    implemented DB with sequelize and created users table

--- src/backend/app.js
+++ src/backend/app.js
@@ -4,6 +4,13 @@ const express = require('express');
 
 const app = express();
 
+const sequelize = require('./util/database');
+
 const server = http.createServer(app);
 
-server.listen(8000);
+const User = require('./models/user')
+
+User.sync();
+
+server.listen(8000)
+
---
+++ src/backend/models/user.js
@@ -0,0 +1,18 @@
+const Sequelize = require('sequelize');
+
+const sequelize = require('../util/database');
+
+const User = sequelize.define('user', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    name: Sequelize.STRING,
+    surname: Sequelize.STRING,
+    email: Sequelize.STRING,
+    role: Sequelize.STRING
+});
+
+module.exports = User;
