65f8ef378a75b152363c820f185688c5fe73a8ee
Hasat Serinkan <hasat.serinkan@ug.bilkent.edu.tr>
2023-06-07 18:40:10 +0200

    some changes

--- src/frontend/components/MainPage/SignInForm.js
+++ src/frontend/components/MainPage/SignInForm.js
@@ -1,8 +1,14 @@
-import { Button, Form, Row, Col, Spinner } from "react-bootstrap";
+import { Button, Form, Row, Col, Modal } from "react-bootstrap";
+import { useState } from "react";
 
 const SignInForm = (props) => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <div className="">
+    <div>
       <Row>
         <Form>
           <Row>
@@ -25,13 +31,47 @@ const SignInForm = (props) => {
           </Row>
           <Row className="d-flex justify-content-center">
             <Col className="d-flex justify-content-center">
-              <Button variant="light" className="mt-3" type="submit">
+              <Button variant="light" className="mt-3" onClick={handleShow}>
                 Forgot Password
               </Button>
             </Col>
           </Row>
         </Form>
       </Row>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header>
+          <Modal.Title>Forgot Password</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Row>
+            <Form>
+              <Row>
+                <Form.Group>
+                  <Form.Label>Your Email</Form.Label>
+                  <Form.Control name="user_name" type="text"></Form.Control>
+                </Form.Group>
+              </Row>
+              <Row>
+                <Col className="col-8">
+                  <Button variant="success" className="mt-3" type="submit">
+                    Send Email
+                  </Button>
+                </Col>
+              </Row>
+            </Form>
+          </Row>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
