f1dbf9f428c77832655aea8db4e57b467371daf9
ZhannaTursunbekova <tursunbekova.zhanna@gmail.com>
2023-06-07 15:00:52 +0300

    added models

--- src/backend/app.js
+++ src/backend/app.js
@@ -4,13 +4,18 @@ const express = require('express');
 
 const app = express();
 
-const sequelize = require('./util/database');
-
 const server = http.createServer(app);
 
-const User = require('./models/user')
+//const sequelize = require('./util/database');
+//sequelize.sync();
+
+const User = require('./models/user');
+const Paper = require('./models/paper');
+const Section = require('./models/section');
 
 User.sync();
+Paper.sync();
+Section.sync();
 
 server.listen(8000)
 
