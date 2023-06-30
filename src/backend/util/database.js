59ed7d0e544e71b54d693bef3e88a818d4dfcb15
Hasat Serinkan <hasat.serinkan@ug.bilkent.edu.tr>
2023-06-30 23:23:29 +0200

    database changed

--- src/backend/util/database.js
+++ src/backend/util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("review-platform", "root", "Qwe220995", {
+const sequelize = new Sequelize("review-platform", "root", "", {
   // review-platform is the name of database
   dialect: "mysql",
   host: "localhost",
